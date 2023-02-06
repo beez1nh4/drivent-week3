5d6cec23be1faf1f08353854501adefd8f45b755
Melissa <melmihomel@gmail.com>
2023-02-06 00:35:58 -0300

    feat: add get hotels controller

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -1,4 +1,5 @@
 import { AuthenticatedRequest } from "@/middlewares";
+import hotelsService from "@/services/hotels-service";
 
 import { Response } from "express";
 import httpStatus from "http-status";
@@ -6,7 +7,7 @@ import httpStatus from "http-status";
 export async function getAllHotels(req: AuthenticatedRequest, res: Response) {
 
   try {
-    const allHotels = await enrollmentsService.getOneWithAddressByUserId();
+    const allHotels = await hotelsService.getAllHotels();
 
     return res.status(httpStatus.OK).send(allHotels);
   } catch (error) {
